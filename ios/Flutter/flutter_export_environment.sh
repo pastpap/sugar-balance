cf082e31a230396f8d7c3fbd4113672d4e171832
nipap <stefan.papin@gmail.com>
2023-04-18 23:04:08 +0200

    updated flutter and packages

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Volumes/Second Drive/Work/02_Flutter/install/flutter"
-export "FLUTTER_APPLICATION_PATH=/Volumes/Second Drive/Work/02_Flutter/projects/sugar-balance"
+export "FLUTTER_APPLICATION_PATH=/Users/stefan.papin/Work/02_Flutter/projects/sugar-balance"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Volumes/Second Drive/Work/02_Flutter/projects/sugar-balance/lib/main.dart"
+export "FLUTTER_TARGET=/Users/stefan.papin/Work/02_Flutter/projects/sugar-balance/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVF
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Volumes/Second Drive/Work/02_Flutter/projects/sugar-balance/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/stefan.papin/Work/02_Flutter/projects/sugar-balance/.dart_tool/package_config.json"
